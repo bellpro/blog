637f48a6b8327c5e844718f34c19bcef3928ec54
bellpro <kju1909020@gmail.com>
2021-11-24 01:05:16 +0900

    initial commit

---
+++ src/test/java/com/bellpro/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellpro.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

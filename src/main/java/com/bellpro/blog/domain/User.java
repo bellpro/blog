04d5689a269af9ac2ab2dae062ec7b41250d77fd
bellpro <kju1909020@gmail.com>
2021-11-24 12:47:39 +0900

    User Entity 완료

--- src/main/java/com/bellpro/blog/domain/User.java
+++ src/main/java/com/bellpro/blog/domain/User.java
@@ -17,18 +17,29 @@ public class User {
     private Long id;                                // 기본키 ID
 
     @Column(nullable = false, unique = true)        // 열 설정 (무조건 입력, 중복 안됨)
-    private String username;                        // 로그인 ID
+    private String username;                        // 닉네임
 
     @Column(nullable = false)                       // 열 설정 (무조건 입력)
-    private String password;                        // 로그인 PW
+    private String password;                        // PW
 
     @Column(nullable = false, unique = true)        // 열 설정 (무조건 입력, 중복 안됨)
     private String email;                           // 이메일
 
-    // 생성자
+    @Column(unique = true)                          // 열 설정 (중복 안됨)
+    private Long kakaoId;                           // 카카오 ID
+
+    // 생성자 (닉네임, 로그인 Pw, 이메일)
     public User(String username, String password, String email){
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    // 생성자 (닉네임, 로그인 Pw, 이메일, 카카오 ID)
+    public User(String username, String password, String email, Long kakaoId){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.kakaoId = kakaoId;
     }
 }
